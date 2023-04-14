f7e6bc42cbd9240fefc6a384ae6cf39b225c3fd2
G-Nishikawa <genki.nishikawa@link-u.co.jp>
2023-04-14 15:54:11 +0900

    info

--- index.php
+++ index.php
@@ -1,17 +1,3 @@
 <?php
-    //var_dump($_SERVER['REQUEST_URI']);
-    //var_dump($_SERVER['REQUEST_URI']);
-    //echo '<br/>';
-    //var_dump('^\/login$');
-
-    //ログイン
-    if(preg_match('<^\/login$>', $_SERVER['REQUEST_URI'])){
-        require './login.php';
-        //header('Location: /kensyu/kensyu/login.php');
-    }elseif(preg_match('<^\/logout$>', $_SERVER['REQUEST_URI'])){
-        require './logout.php';
-        //header('Location: /kensyu/kensyu/logout.php');
-    }
-
-
+	phpinfo();
 ?>
